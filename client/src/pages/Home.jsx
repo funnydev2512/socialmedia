a5c887a746a39bb4feed1ade8f9081a59b167320
Nguyễn Danh Hoàng <hoangnd@vmodev.com>
2023-09-14 21:50:21 +0700

    Update Home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { Topbar, ProfileCard, FriendCard } from '../components'
+import { friends, requests} from "../assets/data"
+import { Link } from 'react-router-dom'
+import { NoProfile } from '../assets'
 
 const Home = () => {
   const {user} = useSelector((state) => state.user)
+  const [friendRequest, setFriendRequest] = React.useState(requests)
+  const [suggestedFriend, setSuggestedFriend] = React.useState(friendRequest)
   return (
     <div className='home w-full px-0 lg:px-10 pb-20 2xl:px-40 bg-bgColor lg:rounded-lg h-screen overflow-hidden'>
       <Topbar/>
@@ -15,9 +20,48 @@ const Home = () => {
           <FriendCard friends={user?.friends} />
         </div>
         {/* CENTER */}
-        <div></div>
+        <div classname="flex-1 h-full bg-primary px-4 flex flex-col gap-6 overflow-y-auto">
+
+        </div>
         {/* Right Side */}
-        <div></div>
+        <div classname="hidden w-1/4 h-full lg:flex flex-col gap-8">
+          {/*FRIEND REQUEST*/}
+          <div className='w-full bg-primary shadow-sm rounded-lg px-6 py-5'>
+              <div className='flex items-center justify-between text-xl text-ascent-1 pb-2 border-b border-black'>
+                  <span>Friend Request</span>
+                  <span>{friendRequest?.length}</span>
+              </div>
+
+              <div className='w-full flex flex-col gap-4 pt-4'>
+                {friendRequest?.map(({_id, requestFrom: from}) => (
+                  <div key={_id} className='flex items-center justify-between'>
+                    <Link
+                      to={"/profile/" + from?._id}
+                      className="w-full flex gap-4 items-center cursor-pointer"
+                    >
+                      <img
+                        src={from?.profileUrl ?? NoProfile}
+                        alt={from?.firstName}
+                        className="w-10 h-10 object-cover rounded-full"
+                      />
+                      <div className="flex-1">
+                        <p className="text-base font-medium text-ascent-1">
+                          {from?.firstName} {from?.lastName}
+                        </p>
+                        <span className="text-sm text-ascent-2">
+                          {from?.profession ?? "No Profession"}
+                        </span>
+                      </div>
+                    </Link>
+                  </div>
+                ))}
+              </div>
+          </div>
+          {/*FRIEND REQUEST*/}
+          <div>
+
+          </div>
+        </div>
       </div>
 
     </div>
